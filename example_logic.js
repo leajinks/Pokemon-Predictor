08f6140df70c779f3526118be9b4738a50c938ae
BeckyK <barimonsterbk@gmail.com>
2023-04-12 20:34:32 -0500

    fix mistake in the comments

--- example_logic.js
+++ example_logic.js
@@ -1,6 +1,6 @@
 function buttonPressed(poke1, poke2){
     //poke1 = poke1['type_1_first', 'type_2_first', 'HP_first', ...etc]
-    //poke2 = poke2['type_1_first', 'type_2_first', 'HP_first', ...etc]
+    //poke2 = poke2['type_1_second', 'type_2_second', 'HP_second', ...etc]
     let url = `/predict/${poke1}/${poke2}`
 
     fetch(url).then(response => response.json()).then(
