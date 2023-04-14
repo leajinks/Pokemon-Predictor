36863c415b1bd3aeadf6d018d85b4e456bb8411f
BeckyK <barimonsterbk@gmail.com>
2023-04-13 21:33:51 -0500

    update js

--- example_logic.js
+++ example_logic.js
@@ -1,15 +1,15 @@
-function buttonPressed(poke1, poke2){
-    //FYI: poke1 = string name of left side dropdown pokemon name
-    //FYI: poke2 = string name of right side dropdown pokemon name
+function buttonPressed(){
+    poke1 = dropdownOne.value()
+    poke2 = dropdownTwo.value()
     let url = `/predict/${poke1}/${poke2}`
 
     fetch(url).then(response => response.json()).then(
         json => {
             if(json[0] == 1){
-                document.getElementById('poke1')
+                document.getElementById('#pkmn1')
                 //etc, do something or call some function to highlight winner here
             } else {
-                document.getElementById('poke2')
+                document.getElementById('#pkmn2')
                 //etc, do something or call some function to highlight winner here
             }
         }
