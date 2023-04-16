6f3d97e4a24010ace1b882639f8ec07ecdf4ce82
dganderson94 <115953195+dganderson94@users.noreply.github.com>
2023-04-16 10:55:33 -0500

    removed leftover bits from old code

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,4 @@
 // Setup
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-const dataPromise = d3.json(url);
 const statList = {
     hp: "HP",
     atk: "Attack",
